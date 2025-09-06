45f719ebfc2d5bbf0cc42efd11b5346caee35975
Lucascec95 <Lucasbresende95@outlook.com>
2025-09-06 15:52:43 +0000

    Super logicaSuperTrunfo

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,22 +1,99 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main() {
+int main(){
+
+    int resultado1, resultado2;
+  char Brasil, Argentina;
+  int ataque1, ataque2, defesa1, defesa2, recuo1, recuo2;
+
+  // gerar numero aleatorio
+  srand(time(0));
+  ataque1 =1;//rand() % 100 + 1; // numero entre 1 e 100
+  ataque2 =0;//rand() % 100 + 1; // numero entre 1 e 100
+  defesa1 =1;//rand() % 100 + 1; // numero entre 1 e 100
+  defesa2 =0;//rand() % 100 + 1; // numero entre 1 e 100
+  recuo1 =0;//rand() % 100 + 1; // numero entre 1 e 100
+  recuo2 =1;//rand() % 100 + 1; // numero entre 1 e 100
+
+  // Inicio de jogo
+
+  printf ("Bem vindo ao jogo\n");
+  printf ("Escolha o Brasil\n");
+  printf("A. ataque\n");
+  printf("D. Defesa\n");
+  printf("R. recuo\n");
+
+  printf("Escolha a comparação: ");
+  scanf("%c", &Brasil);
+
+  switch (Brasil)
+  {
+  case 'A':
+  printf("voce escolheu a opção ataque\n");
+  resultado1 = ataque1 > ataque2 ? 1 : 0; 
+  break;
+  case 'D':
+    printf("voce escolheu a opção defesa\n");
+  resultado1 = defesa1 > defesa2 ? 1 : 0;
+  break;
+  case 'R':
+    printf("voce escolheu a opção recuo\n");
+  resultado1 = recuo1 > recuo2 ? 1 : 0; 
+  break;
+ default:
+ printf("opção de jogo invalida\n");
+ break;
+  
+}
+
+  printf ("escolha a argentina\n");
+  printf ("atenção você deve escolher um atributo diferente do primeiro\n");
+  printf("A. ataque\n");
+  printf("D. Defesa\n");
+  printf("R. recuo\n");
+
+  printf("escolha a comparaçã:");
+  scanf("%c", &Argentina);
+
+  if (Brasil == Argentina)
+  {
+  printf("você escolheu o mesmo atributo");
+  }else{
+       switch (Argentina)
+       {
+       case 'A':
+        printf ("você escolheu a opção ataque\n");
+        resultado2 = ataque1 > ataque2 ? 1 : 0;
+        break;
+       
+        case 'D':
+       printf ("você escolheu a opção defesa\n");
+       resultado2= defesa1 > defesa2 ? 1 : 0;
+        break;
+      
+        case 'R':
+      printf("você escolheu a opção recuo\n");
+      resultado2= recuo1 > recuo2 ? 1 : 0;
+      break;
+      default:
+        printf ("opção de jogo invalida\n");
+        break;     
+
+       }
+
+if (resultado1 && resultado2)
+{
+ printf("parabens você venceu\n");
+
+}else if (resultado1 != resultado2){
+ printf("empatou\n");
+}else {
+  printf("infelizmente você perdeu\n");  
    
-Cadastro da Carta 1:
-Estado: SP
-Código da carta: A1
-Nome da cidade: São Paulo
-População (habitantes): 12325000
-Área (km²): 1521.11 
-PIB (em reais): 56724.32
+} 
 
-Cadastro da Carta 2:
-Estado: RJ
-Código da carta: B1
-Nome da cidade: Rio de Janeiro
-População (habitantes): 6748000
-Área (km²): 1200.3
-PIB (em reais): 44532.91
-
+}
 
 }
